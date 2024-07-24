42143e7d828e8d5884a58c09303586af1cc4330e
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2024-07-24 02:45:03 -0400

    Add user table

---
+++ youthhaven-db/youthhaven-db/schema/tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(255) NOT NULL, 
+    [Role] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [CK_User_Role] CHECK (Role IN ('admin', 'editor', 'writer', 'subscriber'))
+)
