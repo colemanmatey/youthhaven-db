c92f2936ec943d7f26a439b062e83e7d95c0261e
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2024-07-26 02:04:02 -0400

    Add new table for user profiles

--- youthhaven-db/youthhaven-db/schema/tables/User.sql
+++ youthhaven-db/youthhaven-db/schema/tables/User.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Email] NVARCHAR(255) NOT NULL, 
-    [Role] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [CK_User_Role] CHECK (Role IN ('admin', 'editor', 'writer', 'subscriber'))
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(255) NOT NULL, 
+    [Email] NVARCHAR(100) NOT NULL, 
+    [Joined] DATETIME NOT NULL, 
+    [LastLogin] DATETIME NOT NULL 
 )
---
+++ youthhaven-db/youthhaven-db/schema/tables/UserProfile.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[UserProfile]
+(
+	[ProfileID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserID] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [MiddleName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [DateOfBirth] DATE NOT NULL,
+    [Biography] NVARCHAR(300) NULL, 
+    [CountryCode] NCHAR(5) NOT NULL, 
+    [Phone] NCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_UserProfile_User] FOREIGN KEY ([UserID]) REFERENCES [User]([UserID]),
+    CONSTRAINT [UQ_UserProfile_UserID] UNIQUE (UserID)
+)
